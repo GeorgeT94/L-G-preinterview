ce693e38840bf7d26590166e9b9d7ea8c6ece263
CuriouserGeorge <30607279+CuriouserGeorge@users.noreply.github.com>
2019-01-22 15:19:45 +0000

    first commit

---
+++ Tests/BannedWordsTests.py
@@ -0,0 +1,79 @@
+import unittest
+
+import time
+
+import os, sys, io
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from bannedWords.BannedWords import BannedWords
+
+class TestBannedWords(unittest.TestCase):
+
+    def setUp(self):
+        self.bannedWords = BannedWords()
+        self.capturedOutput = io.StringIO()                 
+        sys.stdout = self.capturedOutput
+                 
+    def test_getBannedWordsList(self):
+       
+        self.bannedWords.setBannedWords(fileName ="Tests/BannedWords/testWords.txt")
+
+        self.assertEqual(self.bannedWords.getBannedWordsList(), {"fudge", "secret"}, "Should be {fudge, secret}")
+
+    def test_setBannedWordsList(self):
+
+        self.bannedWords.setBannedWords(fileName="Tests/BannedWords/oneWord.txt")
+
+        self.assertEqual(self.bannedWords.getBannedWordsList(),{'simple'} )
+
+    def test_printSensoredDocument_NoBannedWords(self):
+        self.bannedWords.__bannedWords = {}
+        self.bannedWords.setProseFileName("Tests/Prose/basicText.txt")
+
+        self.bannedWords.printCensoredDocument()
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(self.capturedOutput.getvalue(), "A simple sentence", "should be 'A simple sentence '")
+
+    def test_printSensoredDocument_CensorOneWord(self):
+        
+        self.bannedWords.setBannedWords(fileName="Tests/BannedWords/oneWord.txt")
+       
+        self.bannedWords.setProseFileName(fileName="Tests/Prose/basicText.txt")
+
+        self.bannedWords.printCensoredDocument()
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(self.capturedOutput.getvalue(), "A ****** sentence", "should be 'A ****** sentence'")
+
+    def test_printSensoredDocument_CensorMultipleWords(self):
+        self.bannedWords.setBannedWords("Tests/BannedWords/fudge-secret.txt")
+        self.bannedWords.setProseFileName("Tests/Prose/fudgeProse.txt")
+
+        self.bannedWords.printCensoredDocument()
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(self.capturedOutput.getvalue(),"There are some words that one must not say such as ***** and ****** - everything else is fine!", "should **** secret and fudge" )
+
+    def test_printSensoredDocument_LotsOfBannedWords(self):
+        self.bannedWords.setBannedWords("Tests/BannedWords/lotsOfWords.txt")
+        self.bannedWords.setProseFileName("Tests/Prose/prose.txt")
+
+        startTime = time.time()
+        self.bannedWords.printCensoredDocument()
+        endTime = time.time() - startTime
+
+        self.assertLess(endTime, 0.100)
+
+    def test_printSensoredDocument_LargeTextLotsOfBannedWords(self):
+        self.bannedWords.setBannedWords("Tests/BannedWords/lotsOfWords.txt")
+        self.bannedWords.setProseFileName("Tests/Prose/longProse.txt")
+
+        startTime = time.time()
+        self.bannedWords.printCensoredDocument()
+        endTime = time.time() - startTime
+
+        self.assertLess(endTime, 1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ bannedWords/BannedWords.py
@@ -0,0 +1,44 @@
+import os, sys, io
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import re
+
+class BannedWords():
+
+    __proseFileName = "Tests/Prose/longProse.txt"
+    __bannedWords = set()
+
+    def setProseFileName(self , fileName):
+        self.__proseFileName = fileName
+    
+    def setBannedWords(self, fileName):
+
+        bannedWords = set()
+        with open(fileName) as f:
+            for line in f:
+                if line[-1] == "\n":
+                    line = line[:-1]
+                bannedWords.add(line.lower())
+        
+        self.__bannedWords = bannedWords
+        return 0
+    
+    def getBannedWordsList(self):
+        return self.__bannedWords
+
+    def getProseFileName(self):
+        return self.__proseFileName
+
+    def printCensoredDocument(self):
+
+        with open(self.__proseFileName) as f:
+            for line in f:
+                splitLine = re.findall(r"[\w']+|[ .,!?;-]", line)
+                for word in splitLine:
+                    if word.lower() in self.__bannedWords:
+                        print('*' * len(word), end='')
+                    else:
+                        print(word, end="")
+
+# bannedWords = BannedWords()
+# bannedWords.setBannedWords("Tests/BannedWords/banned_Words.txt")
+# bannedWords.printCensoredDocument()
